a98797da7f05de0d41e5075d286e80dbef25f3b1
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-08-04 23:48:07 +0100

    Add coins api

---
+++ src/API/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const coinAPI = async () => {
+  try {
+    const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
+
+export default coinAPI;
