7df12e5013e2ddb5342d6d80fdad72e93d296ead
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-08-05 15:54:49 +0100

    Create Filter component file

---
+++ src/containers/Filter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Filter() {
+
+  return (
+    <div className="coin-search">
+      <h3>search coin here...</h3>
+    </div>
+  );
+}
+
+export default Filter;
