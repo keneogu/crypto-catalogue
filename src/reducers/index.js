b0a239be18a24336bf8ca3538d08b9fb270d6b09
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-08-05 15:50:03 +0100

    Add filter reducer to combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import coin from './coin';
 import currentCoin from './currentCoin';
-// import filter from './filter';
+import filter from './filter';
 
-export default combineReducers({ coin, currentCoin });
+export default combineReducers({ coin, currentCoin, filter });
