3518c460d3d88fb33b88f9408865e10bf8872f04
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-08-05 15:57:12 +0100

    Add handleChange function to filter form

--- src/containers/Filter.js
+++ src/containers/Filter.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { filterCoin } from '../actions';
 
 function Filter() {
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    dispatch(filterCoin({ value: e.target.value }));
+  };
 
   return (
     <div className="coin-search">
