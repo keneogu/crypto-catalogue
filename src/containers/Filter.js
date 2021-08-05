be196ecd1682b6ddc883cbec2406fcdbbb010737
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-08-05 15:55:10 +0100

    Create Filter form

--- src/containers/Filter.js
+++ src/containers/Filter.js
@@ -5,6 +5,9 @@ function Filter() {
   return (
     <div className="coin-search">
       <h3>search coin here...</h3>
+      <form>
+        <input type="text" placeholder="search coin" className="coin-input" onChange={handleChange} />
+      </form>
     </div>
   );
 }
