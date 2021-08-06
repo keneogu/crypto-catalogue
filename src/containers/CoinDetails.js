74691cdf2b07d84d72d80fd3521794db5e8fb89c
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-08-06 12:55:34 +0100

    Add useSelector to access the selected coin

--- src/containers/CoinDetails.js
+++ src/containers/CoinDetails.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { fetchCoin } from '../API/api';
 import { seeDetails } from '../actions';
 
 function CoinDetails() {
   const { id } = useParams();
+  const currentCoin = useSelector((state) => state.currentCoin);
   const dispatch = useDispatch();
+  console.log(currentCoin);
 
   const fetchCoinDetails = async (id) => {
     const response = await fetchCoin(id);
     dispatch(seeDetails(response));
   };
+
+  useEffect(() => {
+    if (id && id !== '') fetchCoinDetails(id);
+  }, [id]);
 
   return (
     <div>
