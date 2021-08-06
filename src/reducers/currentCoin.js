0f1975705bf8716b0d9584a026cad72d06175cd8
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-08-06 15:00:05 +0100

    Set Initial state of currentReducer to empty object

--- src/reducers/currentCoin.js
+++ src/reducers/currentCoin.js
@@ -1,6 +1,6 @@
 import { COIN_DETAILS } from '../actions/actionType';
 
-const currentCoinReducer = (state = [], action) => {
+const currentCoinReducer = (state = {}, action) => {
   switch (action.type) {
     case COIN_DETAILS:
       return action.payload;
