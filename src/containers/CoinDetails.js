c8c0c1f63075ba5e72b9e0c319c25f2a7d1d2ced
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-08-09 14:36:59 +0100

    Add each coinDetails properties

--- src/containers/CoinDetails.js
+++ src/containers/CoinDetails.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCoin } from '../API/api';
 import { seeDetails } from '../actions';
+import '../styles/CoinDetails.css';
 
 function CoinDetails() {
   const { id } = useParams();
@@ -23,8 +24,38 @@ function CoinDetails() {
       {Object.keys(currentCoin).length === 0 ? (
         <div>...Loading</div>
       ) : (
-        <div>
-          <p>working...</p>
+        <div className="coinDetail-container">
+          <div className="coin-d-row">
+            <div className="coin-left">
+              <h1 className="coin-id">{currentCoin.id}</h1>
+              <img src={currentCoin.image} alt="crypto" />
+            </div>
+            <div className="coin-right">
+              <div className="coin-ds-contain">
+                <h5 className="coin-ds">Name:</h5>
+                <p className="coin-ds-p">{currentCoin.name}</p>
+              </div>
+              <div className="coin-ds-contain">
+                <h5 className="coin-ds">Market Cap rank:</h5>
+                <p className="coin-ds-p">{currentCoin.market_cap_rank.toLocaleString()}</p>
+              </div>
+              <div className="coin-ds-contain">
+                <h5 className="coin-ds">Price:</h5>
+                <p className="coin-ds-p">
+                  <span>$</span>
+                  {currentCoin.current_price.toLocaleString()}
+                </p>
+              </div>
+              <div className="coin-ds-contain">
+                <h5 className="coin-ds">Market Cap:</h5>
+                <p className="coin-ds-p">
+                  <span>$</span>
+                  {currentCoin.market_cap.toLocaleString()}
+                </p>
+              </div>
+              <p className="coin-des">{currentCoin.description}</p>
+            </div>
+          </div>
         </div>
       )}
     </div>
