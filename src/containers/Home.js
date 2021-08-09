17a05f36e3d69813a355a0b19d3f8df272b18da2
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-08-09 21:56:55 +0100

    Add Home page contents

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Typewriter } from 'react-simple-typewriter';
+import '../styles/Home.css';
 
 function Home() {
   return (
-    <div>
-      <h2>Moving good</h2>
+    <div className="home-container">
+      <div className="home-contain">
+        <h1 className="home-h-text">Welcome to Crypto</h1>
+        <h5 className="home-text">
+          {' '}
+          <Typewriter
+            words={['Our mission is to make crypto accessable to everybody']}
+            loop={0}
+            cursor
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+        </h5>
+      </div>
     </div>
   );
 }
